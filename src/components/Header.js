821e97aa9e23901c35c30b62766e35c186013726
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-14 20:46:23 +0900

    added icons to Photo & Header components and styled them

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Cart from "../images/cart.png";
 
 const Header = () => {
   return (
     <header>
       <h1>Pic Some</h1>
+      <img className="cart" src={Cart} alt="cart" />
     </header>
   );
 };
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Heart from "../images/heart.png";
+import RedHeart from "../images/red_heart.png";
+import Add from "../images/add.png";
 
 const Photo = ({ data }) => {
   const photoMap = data.map((el) => {
     return (
-      <img
-        className="image-grid image-container wide tall big"
-        src={el.url}
-        alt={el.id}
-      />
+      <div className="image-container">
+        <img className="favorite" src={Heart} alt="heart" />
+        <img className="image-grid wide tall big" src={el.url} alt={el.id} />
+        <img className="cart" src={Add} alt="add" />
+      </div>
     );
   });
   return <div>{photoMap}</div>;
