5f4b1b5494739ab424907e60ec0257b8f4cf4ec3
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-15 16:42:56 +0900

    added price of each img, a total price and purchase button

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Cart = ({ choosenImg }) => {
+  const UNIT_PRICE = 5.99;
   const cartImg = choosenImg.map((el) => {
+    console.log(el);
     return (
       <div className="cart-item">
         <img className="image-grid" src={el.url} alt={el.id} />
+        <h3>{`$${UNIT_PRICE}`}</h3>
       </div>
     );
   });
@@ -13,6 +16,10 @@ const Cart = ({ choosenImg }) => {
     <main class="cart-page">
       <h1>Check out</h1>
       {cartImg}
+      <h1 className="total-cost">Total: ${UNIT_PRICE * cartImg.length}</h1>
+      <div>
+        <button className="order-button">Place Order</button>
+      </div>
     </main>
   );
 };
