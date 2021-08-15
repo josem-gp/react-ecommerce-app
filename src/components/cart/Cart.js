bccb794e0158f72c6366dd2055a7f7d43fd9ffa7
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-15 16:29:59 +0900

    styled images in the cart page

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,14 +1,20 @@
 import React from "react";
 
-const Cart = ({choosenImg}) => {
+const Cart = ({ choosenImg }) => {
+  const cartImg = choosenImg.map((el) => {
+    return (
+      <div className="cart-item">
+        <img className="image-grid" src={el.url} alt={el.id} />
+      </div>
+    );
+  });
 
-    const cartImg = choosenImg.map((el) => {
-        return (
-            
-        )
-    })
-
-  return <div>Hi there from cart!</div>;
+  return (
+    <main class="cart-page">
+      <h1>Check out</h1>
+      {cartImg}
+    </main>
+  );
 };
 
 export default Cart;
